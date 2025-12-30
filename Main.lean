977ddecaaa14829f658806a745015b87a7b68f54
Sokhibjon Orzikulov <sakhib@orzklv.uz>
2025-12-30 11:04:02 +0500

    chore: first exercise

--- Main.lean
+++ Main.lean
@@ -1,5 +1,18 @@
-def addl (n : Nat) : Nat := n + 1
+def joinStringWith (d: String) (f: String) (s: String) :=
+  String.append f (String.append d s)
+example : String -> String -> String -> String := joinStringWith
+
+#check (joinStringWith ": ")
+#eval joinStringWith " " "some" "thing"
+
+def volume (l: Nat) (w: Nat) (h: Nat) : Nat :=
+  l * w * h
+example : Nat -> Nat -> Nat -> Nat := volume
+
+#check (volume)
+#eval (volume 3 4 5)
+
+
 
 def main : IO Unit := do
-  IO.println "functions go brr"
-  IO.println (addl 2)
+  IO.println "Hello World!"
